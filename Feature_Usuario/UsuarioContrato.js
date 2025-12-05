9597c895c3b76c502e4540e84856fa789acc6c6b
warnellpk <perdomowarnell@gmail.com>
2025-12-05 19:46:06 -0400

    los comandos de usuarios

---
+++ Feature_Usuario/UsuarioContrato.js
@@ -0,0 +1,20 @@
+const ContratosUsuarios = {
+  Normal: [
+    "INICIAR_SESION",
+    "REGISTRAR",
+    "REPORTAR",
+    "VER_DENUNCIAS",
+    "VER_HISTORIAL",
+    "CANCELAR_DENUNCIA",
+    "PUBLICAR_FORO"
+  ],
+
+  Administrador: [
+    "VER_USUARIOS",
+    "ELIMINAR_USUARIO",
+    "BANEAR_USUARIO"
+  ]
+};
+
+module.exports = ContratosUsuarios;
+
