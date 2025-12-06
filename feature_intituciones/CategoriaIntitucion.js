c719ad6eab68890534e4105243a7a92df23898a5
Josvir Herrera <josvirherrera03@gmail.com>
2025-12-05 23:06:33 -0400

    funcion de intituciones

---
+++ feature_intituciones/CategoriaIntitucion.js
@@ -0,0 +1,14 @@
+/**
+ Enumeración para categorías de instituciones gubernamentales.
+ @readonly
+ @enum {string}
+ */
+export const InstitutionCategory = Object.freeze({
+  ELECTRICITY: "electricity",
+  INFRASTRUCTURE: "infrastructure",
+  WATER: "water",
+  MAINTENANCE: "maintenance",
+  SECURITY: "security",
+  TRANSPORT: "transport",
+  OTHER: "other"
+});
